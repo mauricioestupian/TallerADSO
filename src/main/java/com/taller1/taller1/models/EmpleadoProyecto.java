14e372f3a5faccd4b56823154b167d0322b84df7
Mauricio Estupiñan <mauricio_estupinan@yahoo.com>
2025-08-29 15:43:19 -0500

    Modifica Empleado proyecto y agrega mapper

--- src/main/java/com/taller1/taller1/models/EmpleadoProyecto.java
+++ src/main/java/com/taller1/taller1/models/EmpleadoProyecto.java
@@ -1,12 +1,16 @@
 package com.taller1.taller1.models;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -36,4 +40,13 @@ public class EmpleadoProyecto {
     @MapsId("proyectoId")
     private Proyecto proyecto;
     // Getters, setters, etc.
+
+    @NotNull
+    @Column(name = "fecha_asignacion")
+    private LocalDate fechaAsignacion;
+
+    @Size(max = 500)
+    @Column(name = "observaciones")
+    private String observaciones;
+
 }
