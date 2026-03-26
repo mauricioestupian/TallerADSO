6628656ffd5580c8f96bb2afa907ce2e6e02ec8c
Mauricio Estupiñan <mauricio_estupinan@yahoo.com>
2026-03-26 10:35:26 -0500

    reversion en la autentificacion

--- src/main/java/com/taller1/taller1/controllers/EmpleadoController.java
+++ src/main/java/com/taller1/taller1/controllers/EmpleadoController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -67,6 +68,7 @@ public class EmpleadoController {
     // Listar todos
 
     @GetMapping
+    @PreAuthorize("hasRole('Analista de Datos')")
     public ResponseEntity<List<EmpleadoDTO>> listarTodos() {
         List<EmpleadoDTO> empleados = empleadoService.listarTodos();
         return ResponseEntity.ok(empleados);
